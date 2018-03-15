11de318bec98b81f2ff6d99d365f31e83802c6d5
Pranav Raja <rickdangerous1@gmail.com>
2018-03-15 17:22:24 +1100

    customer: Add PaymentMethodNonce
    This is recommended by braintree/paypal

--- customer.go
+++ customer.go
@@ -3,18 +3,19 @@ package braintree
 import "github.com/streamco/braintree-go/nullable"
 
 type Customer struct {
-	XMLName        string          `xml:"customer"`
-	Id             string          `xml:"id,omitempty"`
-	FirstName      string          `xml:"first-name,omitempty"`
-	LastName       string          `xml:"last-name,omitempty"`
-	Company        string          `xml:"company,omitempty"`
-	Email          string          `xml:"email,omitempty"`
-	Phone          string          `xml:"phone,omitempty"`
-	Fax            string          `xml:"fax,omitempty"`
-	Website        string          `xml:"website,omitempty"`
-	CreditCard     *CreditCard     `xml:"credit-card,omitempty"`
-	CreditCards    *CreditCards    `xml:"credit-cards,omitempty"`
-	PayPalAccounts *PayPalAccounts `xml:"paypal-accounts,omitempty"`
+	XMLName            string          `xml:"customer"`
+	Id                 string          `xml:"id,omitempty"`
+	FirstName          string          `xml:"first-name,omitempty"`
+	LastName           string          `xml:"last-name,omitempty"`
+	Company            string          `xml:"company,omitempty"`
+	Email              string          `xml:"email,omitempty"`
+	Phone              string          `xml:"phone,omitempty"`
+	Fax                string          `xml:"fax,omitempty"`
+	Website            string          `xml:"website,omitempty"`
+	PaymentMethodNonce string          `xml:"payment-method-nonce"`
+	CreditCard         *CreditCard     `xml:"credit-card,omitempty"`
+	CreditCards        *CreditCards    `xml:"credit-cards,omitempty"`
+	PayPalAccounts     *PayPalAccounts `xml:"paypal-accounts,omitempty"`
 }
 
 // PaymentMethods returns a slice of all PaymentMethods this customer has
